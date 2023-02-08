ee66b7f097a9c863618661c9f8a6ab882a028a45
Diktator <dychervony@gmail.com>
2023-02-08 13:38:42 +0200

    добивл лабу за 08.02.2023

---
+++ Laba08.02.2023/Laba08.02.2023/CreditCard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba08._02._2023 {
+    internal class CreditCard {
+        string card_num, name, surname, pathronymic, card_date, cvc;
+        internal CreditCard() => card_num = name = surname = pathronymic = cvc = null;
+        internal CreditCard(CreditCard obj) {
+            card_num = obj.card_num;
+            name = obj.name;
+            surname = obj.surname;
+            pathronymic= obj.pathronymic;
+            cvc = obj.cvc;
+        }
+        internal void Input() {
+            try {
+                Console.Write("Введите номер карты: ");
+                card_num = Console.ReadLine();
+                if(card_num.Length != 16){
+                    throw new Exception("Исключение: Введена неправильная длина номера карты.");
+                }
+            }
+            catch(Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+            Console.Write("Введите имя владельца: ");
+            name = Console.ReadLine();
+            Console.Write("Введите фамилию владельца: ");
+            surname = Console.ReadLine();
+            Console.Write("Введите отчество владельца: ");
+            pathronymic = Console.ReadLine();
+            try {
+                Console.Write("Введите cvc-код: ");
+                cvc = Console.ReadLine();
+                if (cvc.Length != 3) {
+                    throw new Exception("Исключение: Введена неправильная длина CVC-кода.");
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Write("Введите дату окончания работы карты: ");
+            card_date = Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Laba08.02.2023/Laba08.02.2023/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba08._02._2023 {
+    internal class Program {
+        static void Main(string[] args) {
+            CreditCard obj = new CreditCard();
+            obj.Input();
+            Console.ReadKey();
+        }
+    }
+}
