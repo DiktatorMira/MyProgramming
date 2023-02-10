9b8cb96d634b65f67c7a588554571e6966378c9d
Diktator <dychervony@gmail.com>
2023-02-10 12:02:08 +0200

    добавил дз за 10.02.2023

---
+++ Dz10.02.2023/Dz10.02.2023/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz10._02._2023 {
+    internal class Book {
+        string book_name, author_name;
+        int pages;
+        public string BookName {
+            get { return book_name; }
+            set { book_name = value; }
+        }
+        public string AuthorName {
+            get { return author_name; }
+            set { author_name = value; }
+        }
+        public int Pages {
+            get { return pages; }
+            set { pages = value; }
+        }
+        internal Book() {
+            book_name = author_name = null;
+            pages = 0;
+        }
+        internal Book(Book obj) {
+            book_name = obj.book_name;
+            author_name = obj.author_name;
+            pages = obj.pages;
+        }
+        internal void Input() {
+            Console.Write("Введите название книги: ");
+            book_name = Console.ReadLine();
+            Console.Write("Введите ФИО автора книги: ");
+            author_name = Console.ReadLine();
+            Console.Write("Введите кол-во страниц в книге: ");
+            pages = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+        }
+        internal void Print() {
+            Console.WriteLine($"Название книги: {book_name}");
+            Console.WriteLine($"Имя автора книги: {author_name}");
+            Console.WriteLine($"Кол-во страниц: {pages}");
+        }
+        public override string ToString() {
+            return $"Название книги: {book_name}\nИмя автора книги: {author_name}\nКол-во страниц: {pages}";
+        }
+    }
+}
---
+++ Dz10.02.2023/Dz10.02.2023/BookList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz10._02._2023 {
+    internal class BookList {
+        Book[] books;
+        internal BookList() => books = new Book[0];
+        internal BookList(int size) => books = new Book[size];
+        internal BookList(params Book[] arr) {
+            books = new Book[arr.Length];
+            for (short i = 0; i < arr.Length; i++)
+                books[i] = arr[i];
+        }
+        public Book this[int index] {
+            get {
+                if(index >= 0 && index < books.Length) return books[index];
+                else throw new Exception($"Некорректный индекс: {index}!");
+            }
+            set {
+                if (index >= 0 && index < books.Length) books[index] = value;
+                else throw new Exception($"Некорректный индекс: {index}!");
+            }
+        }
+        internal void AddBook(Book obj) {
+            BookList NewBook = new BookList(books.Length + 1);
+            NewBook[books.Length - 1] = obj;
+        }
+        internal void DelBook(Book obj, int index) {
+            for(int i = index; i < books.Length; i++)
+                (books[i], books[i + 1]) = (books[i + 1], books[i]);
+            books = new Book[books.Length - 1];
+        }
+        internal bool Find(Book obj, string FindName) {
+            if (obj.BookName == FindName) return true;
+            else return false;
+        }
+    }
+}
