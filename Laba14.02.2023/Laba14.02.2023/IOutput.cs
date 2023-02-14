965cf1b8fee022aae0fa6105010ed29ca472a78f
Diktator <dychervony@gmail.com>
2023-02-14 11:05:47 +0200

    добавил лабу за 14.02.2023(ненавижу этот праздник)

---
+++ Laba14.02.2023/Laba14.02.2023/IOutput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba14._02._2023 {
+    internal interface IOutput {
+        void Show();
+        void Show(string info);
+    }
+    internal interface IMath {
+        int Max();
+        int Min();
+        double Avg();
+        bool Search(int find);
+    }
+    internal interface ISort {
+        void SortAsc();
+        void SortDesc();
+        void SortByParam(bool is_asc);
+    }
+}
