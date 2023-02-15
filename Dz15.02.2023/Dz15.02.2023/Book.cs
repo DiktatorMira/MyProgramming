40a819559fd69b815205a6217b1d656c535da93a
Diktator <dychervony@gmail.com>
2023-02-15 14:49:52 +0200

    добавил дз за 15.02.2023

---
+++ Dz15.02.2023/Dz15.02.2023/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dz15._02._2023 {
+    internal class Book : IComparable, ICloneable {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Descripion { get; set; }
+        public int Pages { get; set; }
+        public Book() :this(null, null, null, 0) { }
+        public Book(string name, string author, string desc, int pages) {
+            Name = name;
+            Author = author;
+            Descripion = desc;
+            Pages = pages;
+        }
+        public void Input() {
+            Console.Write("Введите название книги: ");
+            Name = Console.ReadLine();
+            Console.Write("Введите автора книги: ");
+            Author = Console.ReadLine();
+            Console.Write("Введите описание книги: ");
+            Descripion = Console.ReadLine();
+            Console.Write("Введите кол-во страниц в книге: ");
+            Pages = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+        }
+        public void Print() {
+            Console.WriteLine($"Книга: {Name}");
+            Console.WriteLine($"Автор книги: {Author}");
+            Console.WriteLine($"Описание: {Descripion}");
+            Console.WriteLine($"Кол-во страниц: {Pages}");
+        }
+        public int CompareTo(object obj) {
+            if (obj is Book) return Name.CompareTo((obj as Book).Name);
+            throw new NotImplementedException();
+        }
+        public object Clone() { return new Book(Name, Author, Descripion, Pages); }
+    }
+    internal class SortBookName : IComparer {
+        int IComparer.Compare(object obj1, object obj2) {
+            if (obj1 is Book && obj2 is Book)  return (obj1 as Book).Name.CompareTo((obj2 as Book).Name);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dz15.02.2023/Dz15.02.2023/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz15._02._2023 {
+    internal class Library {
+        Book[] books;
+        int position = -1;
+        public Library() => books = null;
+        public Library(Book[] books) {
+            books = new Book[books.Length];
+            for (short i = 0; i < books.Length; i++)
+                books[i] = new Book(books[i].Name, books[i].Author, books[i].Descripion, books[i].Pages);
+        }
+        public void InputBooks() {
+            for (short i = 0; i < books.Length; i++) books[i].Input();
+        }
+        public void PrintBooks() {
+            for (int i = 0; i < books.Length; i++) books[i].Print();
+        }
+        public IEnumerator GetEnumerator() {
+            for (int i = 0; i < books.Length; i++) yield return books[i];
+        }
+    }
+}
