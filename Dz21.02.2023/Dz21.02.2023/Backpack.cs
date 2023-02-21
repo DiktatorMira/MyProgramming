377ba64b310ce95f1e8f93bf3d5adf9ab7d33404
Diktator <dychervony@gmail.com>
2023-02-21 12:23:55 +0200

    добавил дз за 21.02.2023

---
+++ Dz21.02.2023/Dz21.02.2023/Backpack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz21._02._2023 {
+    delegate void MyDlg(string new_item, MyDlg dlg);
+    internal class Backpack {
+        public string Color { get; set; }
+        public string Firma { get; set; }
+        public string Cloth { get; set; }
+        public string Weight { get; set; }
+        public string Capacity { get; set; }
+        public string[] Content { get; set; }
+        public Backpack():this(null, null, null, null, null, null) { }
+        public Backpack(string color, string firma, string cloth, string weight, string capacity, string[] content) {
+            Color = color;
+            Firma = firma;
+            Cloth = cloth;
+            Weight = weight;
+            Capacity = capacity;
+            for(short i = 0; i < content.Length; i++)
+                Content[i] = content[i];
+        }
+        public void AddObject(string new_item, MyDlg dlg) {
+            string[] temp = new string[Content.Length];
+            for(short i = 0; i < Content.Length; i++)
+                temp[i] = Content[i];
+            Content = new string[Content.Length + 1];
+            for (short i = 0; i < Content.Length; i++) {
+                if (i == Content.Length - 1) Content[i] = new_item;
+                Content[i] = temp[i];
+            }
+        }
+    }
+}
---
+++ Dz21.02.2023/Dz21.02.2023/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz21._02._2023 {
+    delegate void MyDelegate1(int num1, int num2, int num3);
+    delegate int MyDelegate2(int[] arr);
+    delegate bool MyDelegate3(string text, string find);
+    internal class Program {
+        static MyDelegate1 rgb = (int num1, int num2, int num3) => {
+            Console.WriteLine($"Ваш цвет RGB: {num1} {num2} {num3}");
+        };
+        static MyDelegate2 task3 = (int[] arr) => {
+            int quan = 0;
+            for(Int16 i = 0; i < arr.Length; i++)
+                if (arr[i] % 7 == 0) quan++;
+            return quan;
+        };
+        static MyDelegate2 task4 = (int[] arr) => {
+            int quan = 0;
+            for (Int16 i = 0; i < arr.Length; i++)
+                if (arr[i] >= 0) quan++;
+            return quan;
+        };
+        static MyDelegate2 task5 = (int[] arr) => {
+            int quan = 0;
+            for (Int16 i = 0; i < arr.Length; i++)
+                if (arr[i] < 0) quan++;
+            return quan;
+        };
+        static MyDelegate3 task6 = (string text, string find) => {
+            if (text.Contains(find)) return true;
+            else return false;
+        };
+        static void Main(string[] args) {
+
+        }
+    }
+}
