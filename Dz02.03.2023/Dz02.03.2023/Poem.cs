96d904b0395cd76054d343f7f0ce7b82d044e8b7
Diktator <dychervony@gmail.com>
2023-03-02 13:36:11 +0200

    добавил дз и лабу за 02.03.2023

---
+++ Dz02.03.2023/Dz02.03.2023/Poem.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz02._03._2023 {
+    internal class Poem {
+        public string Name { get; set; }
+        public string AuthorFIO { get; set; }
+        public int Year { get; set; }
+        public string Topik { get; set; }
+        public string Text { get; set; }
+        public Poem() :this(null, null, 0, null) => Text = null;
+        public Poem(string name, string fio, int year, string topik) {
+            Name = name;
+            AuthorFIO = fio;
+            Year = year;
+            Topik = topik;
+            StreamReader file = new StreamReader("Poem.log", Encoding.UTF8);
+            Text = file.ReadToEnd();
+            file.Close();
+        }
+        public void NewTextPoem(string text) {
+            StreamWriter file = new StreamWriter("Poem.log", false);
+            file.Write(text);
+            file.Close();
+        }
+    }
+    internal class PoemList {
+        Poem[] poems;
+        public PoemList() => poems = null;
+        public PoemList(Poem[] arr) {
+            poems = new Poem[arr.Length];
+            for(int i = 0; i < arr.Length; i++) poems[i] = arr[i];
+        }
+        public void AddPoem(Poem poem) {
+            Poem[] temp = poems;
+            poems = new Poem[temp.Length + 1];
+            for(int i = 0; i < poems.Length; i++) {
+                if (i == poems.Length - 1) poems[i] = poem;
+                else poems[i] = temp[i];
+            }
+        }
+        public void DeletePoem(int index) {
+            poems[index] = null;
+            Poem[] temp = poems;
+            poems = new Poem[temp.Length - 1];
+            for(int i = 0; i < poems.Length; i++) {
+                if (temp[i] == null) continue;
+                poems[i] = temp[i];
+            }
+        }
+        public void ChangePoem(int index) {
+            Console.Write("Введите новое название стиха: ");
+            poems[index].Name = Console.ReadLine();
+            Console.Write("Введите новое ФИО автора: ");
+            poems[index].AuthorFIO = Console.ReadLine();
+            Console.Write("Введите новый год написания: ");
+            poems[index].Year = int.Parse(Console.ReadLine());
+            Console.Write("Введите новую тему стиха: ");
+            poems[index].Topik = Console.ReadLine();
+            Console.Write("Введите название нового файла текста стиха: ");
+            string NewFileName = Console.ReadLine();
+            StreamReader file = new StreamReader(NewFileName, Encoding.UTF8);
+            poems[index].Text = file.ReadToEnd();
+            file.Close();
+        }
+        public void FindTopikPoem(string topik) {
+            bool IsFind = false;
+            for(int i = 0; i < poems.Length; i++) {
+                if (poems[i].Topik == topik){
+                    Console.Write(poems[i].Text);
+                    IsFind = true;
+                }
+            }
+            if (IsFind == false) Console.WriteLine("Стихи с заданной темой не найдены.");
+        }
+        public void SaveToFile() {
+            StreamWriter file = new StreamWriter("Poem collections.log", false);
+            for(int i = 0; i < poems.Length; i++) {
+                file.Write($"Название стиха: {poems[i].Name}\n");
+                file.Write($"ФИО автора: {poems[i].AuthorFIO}\n");
+                file.Write($"Год написания: {poems[i].Year}\n");
+                file.Write($"Тема стиха: {poems[i].Topik}\n");
+                file.Write($"Текст стиха:\n {poems[i].Text}\n\n");
+            }
+            file.Close();
+            Console.WriteLine("Коллекция стихов успешно сохранена!");
+        }
+        public void LoadFromFile() {
+            StreamReader file = new StreamReader("\"Poem collections.log", Encoding.UTF8);
+            int quan = 0;
+            for(int i = 0; i < poems.Length; i++) {
+                while(file.Peek() != 0) {
+                    poems[i].Name = file.ReadLine();
+                    poems[i].AuthorFIO = file.ReadLine();
+                    poems[i].Year = int.Parse(file.ReadLine());
+                    poems[i].Topik = file.ReadLine();
+                    poems[i].Text = file.ReadLine();
+                }
+                poems[i] = null;
+                quan++;
+            }
+            file.Close();
+            Poem[] temp = poems;
+            poems = new Poem[temp.Length - quan];
+            for(int i = 0; i < poems.Length; i++)
+                if (temp[i] != null) poems[i] = temp[i];
+        }
+    }
+}
---
+++ Laba02.03.2023/Laba02.03.2023/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Laba02._03._2023 {
+    internal class Program {
+        static void Task1() {
+            int[] Nums = new int[100];
+            Random rand = new Random();
+            for(int i = 0; i < Nums.Length; i++)
+                Nums[i] = rand.Next(1, 100);
+            StreamWriter file1 = new StreamWriter("Simple numbers.log", false);
+            foreach(int i in Nums) {
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                    if (i % j == 0) break;
+                    else file1.Write(i + " ");
+            }
+            file1.Close();
+            StreamWriter file2 = new StreamWriter("Fibonacci numbers.log", false);
+            foreach(int i in Nums) {
+                int j, y, z, b;
+                y = z = b = 1;
+                bool q = false;
+                for(int k = 0; k < Nums[i]; k++) {
+                    z = y;
+                    y = b;
+                    b = z + y;
+                    if (b == Nums[i]) q = true;
+                }
+                if(q) file2.Write(i + " ");
+            }
+            file2.Close();
+            foreach(int i in Nums) Console.Write(i + " ");
+        }
+        static void Task2() {
+            StreamReader file = new StreamReader("Text.txt", Encoding.UTF8);
+            Console.Write("Введите слово для поиска в файле: ");
+            string find = Console.ReadLine();
+            Console.Write("Введите слово для подмены: ");
+            string change = Console.ReadLine();
+            string text = "";
+            while(file.Peek() >= 0) {
+                Console.WriteLine(file.ReadLine());
+                text += file.ReadLine();
+            }
+            file.Close();
+            text = text.Replace(find, change);
+            Console.WriteLine(text);
+        }
+        static void Task3() {
+            StreamReader modfile = new StreamReader("Moder.txt", Encoding.UTF8);
+            string moder = modfile.ReadLine();
+            string[] ModWords = moder.Split(new char[] { ' ' });
+            StreamReader file = new StreamReader("Text2.txt", Encoding.UTF8);
+            string result = file.ReadToEnd();
+            modfile.Close();
+            file.Close();
+            string zvezd = "";
+            for(int i = 0; i < ModWords[0].Length; i++) zvezd += "*";
+            for(int i = 0; i < result.Length; i++)  {
+                if (result.Contains(ModWords[0]))
+                    result = result.Replace(ModWords[0], zvezd);
+            }
+            zvezd = string.Empty;
+            for (int i = 0; i < ModWords[1].Length; i++) zvezd += "*";
+            for (int i = 0; i < result.Length; i++) {
+                if (result.Contains(ModWords[1]))
+                    result = result.Replace(ModWords[1], zvezd);
+            }
+            Console.WriteLine(result);
+        }
+        static void Task4() {
+            StreamReader file = new StreamReader("Text3.txt", Encoding.UTF8);
+            string text = file.ReadLine();
+            file.Close();
+            Console.WriteLine(text);
+            string result = new string(text.ToCharArray().Reverse().ToArray());
+            Console.WriteLine(result);
+            StreamWriter reverse = new StreamWriter("Reverse.log", false);
+            reverse.WriteLine(result);
+            reverse.Close();
+        }
+        static void Task5() {
+            StreamWriter file = new StreamWriter("Numbers.log", false);
+            Random rand = new Random();
+            for(int i = 0; i <= 100000; i++) file.Write(rand.Next(-99999, 99999) + " ");
+            file.Close();
+            StreamReader nums = new StreamReader("Numbers.log", Encoding.UTF8);
+            StreamWriter pos = new StreamWriter("Positive numbers.log", false);
+            StreamWriter neg = new StreamWriter("Negative numbers.log", false);
+            StreamWriter two = new StreamWriter("Two-digit numbers.log", false);
+            StreamWriter five = new StreamWriter("Five-digit numbers.log", false);
+            string numbers = nums.ReadToEnd();
+            nums.Close();
+            string[] NumArr = numbers.Split(new char[] { ' ' });
+            long[] Numbers = new long[NumArr.Length];
+            long positive = 0, negative = 0, two_digit = 0, five_digit = 0;
+            for (int i = 0; i < NumArr.Length; i++) {
+                Numbers[i] = Convert.ToInt64(Numbers[i]);
+                if (Numbers[i] > 0) {
+                    pos.Write(Numbers[i] + " ");
+                    positive++;
+                }
+                if (Numbers[i] < 0) {
+                    neg.Write(Numbers[i] + " ");
+                    negative++;
+                }
+                if (Numbers[i] >= 10 && Numbers[i] <= 99 && Numbers[i] >= -99 && Numbers[i] <= -10) {
+                    two.Write(Numbers[i] + " ");
+                    two_digit++;
+                }
+                if (Numbers[i] >= 10000 && Numbers[i] <= 99999 && Numbers[i] >= -99999 && Numbers[i] <= -10000) {
+                    five.Write(Numbers[i] + " ");
+                    five_digit++;
+                }
+            }
+            pos.Close();
+            neg.Close();
+            two.Close();
+            five.Close();
+            Console.WriteLine($"Количество положительный чисел: {positive}");
+            Console.WriteLine($"Количество отрицательных чисел: {negative}");
+            Console.WriteLine($"Количество двузначных чисел: {two_digit}");
+            Console.WriteLine($"Количество пятизначных чисел: {five_digit}");
+        }
+        static void Main(string[] args) {
+            //Task1();
+            //Task2();
+            //Task3();
+            //Task4();
+            Task5();
+            Console.ReadKey();
+        }
+    }
+}
