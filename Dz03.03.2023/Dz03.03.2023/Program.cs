44b5bf04dacf3e19ab022378509923936a5a12b4
Diktator <dychervony@gmail.com>
2023-03-03 11:53:59 +0200

    добавил дз и лабу за 03.03.2023

---
+++ Dz03.03.2023/Dz03.03.2023/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Dz03._03._2023 {
+    internal class Program {
+        static void Task1() {
+            string str = "ahb acb aeb aeeb adcb axeb";
+            string pattern = @"[a]\w[b]";
+            Regex temp = new Regex(pattern);
+            Match matches = temp.Match(str);
+            while(matches.Success) {
+                Console.Write(matches.Groups[1].Value + " ");
+                Console.Write(matches.Value + " ");
+                matches = matches.NextMatch();
+            }
+        }
+        static void Task2() {
+            string str = "aba aca aea abba adca abea";
+            string pattern = @"[a]\w{2}[a]";
+            Regex temp = new Regex(pattern);
+            Match matches = temp.Match(str);
+            while (matches.Success) {
+                Console.Write(matches.Groups[1].Value + " ");
+                Console.Write(matches.Value + " ");
+                matches = matches.NextMatch();
+            }
+        }
+        static void Task3() {
+            string str = "aba aca aea abba adca abea";
+            string pattern = @"[a]\w[^c][a]";
+            Regex temp = new Regex(pattern);
+            Match matches = temp.Match(str);
+            while (matches.Success) {
+                Console.Write(matches.Groups[1].Value + " ");
+                Console.Write(matches.Value + " ");
+                matches = matches.NextMatch();
+            }
+        }
+        static void Task4() {
+            string str = "aa aba abba abbba abca abea";
+            string pattern = @"[a][b]+[a]";
+            Regex temp = new Regex(pattern);
+            Match matches = temp.Match(str);
+            while (matches.Success) {
+                Console.Write(matches.Groups[1].Value + " ");
+                Console.Write(matches.Value + " ");
+                matches = matches.NextMatch();
+            }
+        }
+        static void Task5() {
+            string str = "aa aba abba abbba abca abea";
+            string pattern = @"[a][b]*[a]";
+            Regex temp = new Regex(pattern);
+            Match matches = temp.Match(str);
+            while (matches.Success) {
+                Console.Write(matches.Groups[1].Value + " ");
+                Console.Write(matches.Value + " ");
+                matches = matches.NextMatch();
+            }
+        }
+        static void Task6() {
+            string str = "aa aba abba abbba abca abea";
+            string pattern = @"[a][b]{0,1}[a]";
+            Regex temp = new Regex(pattern);
+            Match matches = temp.Match(str);
+            while (matches.Success) {
+                Console.Write(matches.Groups[1].Value + " ");
+                Console.Write(matches.Value + " ");
+                matches = matches.NextMatch();
+            }
+        }
+        static void Task7() {
+            string str = "aa aba abba abbba abca abea";
+            string pattern = @"[a][b]*[a]";
+            Regex temp = new Regex(pattern);
+            Match matches = temp.Match(str);
+            while (matches.Success) {
+                Console.Write(matches.Groups[1].Value + " ");
+                Console.Write(matches.Value + " ");
+                matches = matches.NextMatch();
+            }
+        }
+        static void Task8() {
+            string str = "ab abab abab abababab abea";
+            string pattern = @"[ab]+";
+            Regex temp = new Regex(pattern);
+            Match matches = temp.Match(str);
+            while (matches.Success){
+                Console.Write(matches.Groups[1].Value + " ");
+                Console.Write(matches.Value + " ");
+                matches = matches.NextMatch();
+            }
+        }
+        static void Main(string[] args) {
+            //Task1();
+            //Task2();
+            //Task3();
+            //Task4();
+            //Task5();
+            //Task6();
+            //Task7();
+            //Task8();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba03.03.2023/Laba03.03.2023/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Laba03._03._2023 {
+    internal class Program {
+        static void ExtraTask() {
+            Console.Write("Напишите эл. почту: ");
+            string text = Console.ReadLine();
+            string pattern = @"^(\D)(\d)@gmail.com$";
+            Regex temp = new Regex(pattern);
+            if (temp.IsMatch(text)) Console.WriteLine("Введено правильно!");
+            else Console.WriteLine("Неправильный формат эл. почты!");
+            //-------------------------------------------------------
+            text = "+38098-499-40-84";
+            string text2 = "38098-499-40-84";
+            pattern = @"^(+380)(\d{2})(-)(\d{3})(-)(\d{2})(-)(\d{2})";
+            temp = new Regex(pattern);
+            if (temp.IsMatch(text)) Console.WriteLine("Введено правильно!");
+            else Console.WriteLine("Неправильный формат эл. почты!");
+        }
+        static void Task1() {
+            Console.Write("Введите IP: ");
+            string ip = Console.ReadLine();
+            string pattern = @"(\d{3})([.])(\d{3})([.])(\d{3})([.])(\d{3})";
+            Regex temp = new Regex(pattern);
+            if (temp.IsMatch(ip)) Console.WriteLine("Правильно.");
+            else Console.WriteLine("Неправильно.");
+        }
+        static void Task2() {
+            Console.WriteLine("Введите: ");
+            string text = Console.ReadLine();
+            string glas = "ЯУЮОЕЁЭИЫяуюоеёэиы";
+            int count = 0;
+            for(int i = 0; i < text.Length; i++) {
+                for(int j = 0; j < glas.Length; j++)
+                    if (text[i] == glas[j]) count++;
+            }
+            Console.WriteLine($"Кол-во гласных букв: {count}.");
+        }
+        static void Task3() {
+            Console.WriteLine("Введите url: ");
+            string url = Console.ReadLine();
+            string pattern = @"(\w{2,3})([.])(\w+)(\w{2,4})([/])(\w+)([/])(\w+)";
+            Regex temp = new Regex(pattern);
+            if (temp.IsMatch(url)) Console.WriteLine("Правильно.");
+            else Console.WriteLine("Неравильно.");
+        }
+        static void Task4() {
+            Console.WriteLine("Введите текст: ");
+            string text = Console.ReadLine();
+            string pattern = @"\w+";
+            Regex temp = new Regex(pattern);
+            if (temp.IsMatch(text)) Console.WriteLine("Правильно.");
+            else Console.WriteLine("Неравильно.");
+        }
+        static void Task5() {
+            Console.WriteLine("Введите время: ");
+            string time = Console.ReadLine();
+            string pattern = @"^(([01][0-9])|([02][0-3]))(:|\.)([0-5][0-9])(:|\.)([0-5][0-9])$";
+            Regex temp = new Regex(pattern);
+            if (temp.IsMatch(time)) Console.WriteLine("Правильно.");
+            else Console.WriteLine("Неравильно.");
+        }
+        static void Task6() {
+            Console.WriteLine("Введите почтовый индекс США: ");
+            string index = Console.ReadLine();
+            string pattern = @"^(\d{1,5})(-)(\d{1,4})$";
+            Regex temp = new Regex(pattern);
+            if (temp.IsMatch(index)) Console.WriteLine("Правильно.");
+            else Console.WriteLine("Неравильно.");
+        }
+        static void Main(string[] args) {
+            //ExtraTask();
+            //Task1();
+            //Task2();
+            //Task3();
+            //Task4();
+            //Task5();
+            //Task6();
+            Console.ReadKey();
+        }
+    }
+}
