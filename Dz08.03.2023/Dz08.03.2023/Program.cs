4221d3eda49ddeb036217e88e6d17d3c2c5bb807
Diktator <dychervony@gmail.com>
2023-03-08 14:13:06 +0200

    добавил дз за 08.03.2023

---
+++ Dz08.03.2023/Dz08.03.2023/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Dz08._03._2023.Program;
+
+namespace Dz08._03._2023 {
+    internal class Program {
+        public class Device {
+            public string Name { get; set; }
+            public string Firm { get; set; }
+            public int Price { get; set; }
+            public Device() :this(null, null, 0) { }
+            public Device(string name, string firm, int price) {
+                Name = name;
+                Firm = firm;
+                Price = price;
+            }
+        }
+        public class Sort : IEqualityComparer<Device> {
+            public bool Equals(Device x, Device y) {
+                if (x.Name == y.Name) return true;
+                return false;
+            }
+            public int GetHashCode(Device obj)  { return obj.Name.GetHashCode(); }
+        }
+        static void Main(string[] args) {
+            Device obj1 = new Device("Lumia 1", "Nokia", 1200);
+            Device obj2 = new Device("Lumia 3", "Nokia", 1800);
+            Device obj3 = new Device("9 pro", "One Plus", 15000);
+            Device[] mass1 = { obj1, obj2, obj3 };
+            Device obj4 = new Device("X", "Iphone", 20000);
+            Device obj5 = new Device("Mi Note 10 lite", "Xiaomi", 8000);
+            Device obj6 = new Device("S20", "Samsung", 25000);
+            Device[] mass2 = { obj1, obj2, obj3 };
+            var MinusArr = mass1.Except(mass2);
+            var PeresechArr = mass1.Intersect(mass2);
+            var UniteArr = mass1.Union(mass2);
+        }
+    }
+}
