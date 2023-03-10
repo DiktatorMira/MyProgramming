cedc54b29c948f2f3aed4e457d395910ed8ccc26
Diktator <dychervony@gmail.com>
2023-03-10 10:36:16 +0200

    обновли дз за 08.03.2023

--- Dz08.03.2023/Dz08.03.2023/Program.cs
+++ Dz08.03.2023/Dz08.03.2023/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 using static Dz08._03._2023.Program;
+using System.Security.Policy;
+using System.IO;
+
 
 namespace Dz08._03._2023 {
     internal class Program {
+        [Serializable]
+        [DataContact]
         public class Device {
+            [DataMember]
             public string Name { get; set; }
+            [DataMember]
             public string Firm { get; set; }
+            [DataMember]
             public int Price { get; set; }
             public Device() :this(null, null, 0) { }
             public Device(string name, string firm, int price) {
@@ -18,6 +27,8 @@ namespace Dz08._03._2023 {
                 Price = price;
             }
         }
+        [KnownType(typeof(Device))]
+        [XmlInclude(typeof(Device))]
         public class Sort : IEqualityComparer<Device> {
             public bool Equals(Device x, Device y) {
                 if (x.Name == y.Name) return true;
@@ -37,6 +48,18 @@ namespace Dz08._03._2023 {
             var MinusArr = mass1.Except(mass2);
             var PeresechArr = mass1.Intersect(mass2);
             var UniteArr = mass1.Union(mass2);
+            List<int> l = new List<int>();
+            FileStream stream = new FileStream("../../list.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<int>));
+            serializer.Serialize(stream, l);
+            stream.Close();
+            Console.WriteLine("Сериализация успешно выполнена!");
+            stream = new FileStream("../../data.json", FileMode.Create);
+            Device obj7 = new Device("S20", "Samsung", 25000);
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Device));
+            jsonFormatter.WriteObject(stream, obj7);
+            stream.Close();
+            Console.WriteLine("Сериализация успешно выполнена!");
         }
     }
 }
