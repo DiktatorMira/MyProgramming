247b32ae15b55c45f0642c1e463ed3a4f20e6bcb
Diktator <dychervony@gmail.com>
2023-03-14 08:55:08 +0200

    обновил проект

--- Project1/Project1/EngRus.cs
+++ Project1/Project1/EngRus.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,29 +24,65 @@ namespace Project1 {
             Count = MyDictionary.Count;
         }
         public void Input() {
+            bool IsRepeat = false;
             do {
-                Console.Write("Добавьте новое слово на англ: ");
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.Write("\nДобавьте новое слово в словарь: ");
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 string text1 = Console.ReadLine();
-                Console.Write("Добавьте вариант переводана русский: ");
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.Write("Добавьте вариант(ы) перевода слова: ");
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 string text2 = Console.ReadLine();
-                MyDictionary.Add(Tuple.Create(text1, text2));
+                if((text1 == "Паляниця" && text2 == "клубника") || (text1 == "Клубника" && text2 == "паляниця")) {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\t==>|До вас прямує наряд СБУ!");
+                }
+                for(int i = 0; i < MyDictionary.Count; i++) {
+                    if (MyDictionary[i].Item1 == text1) {
+                        MyDictionary[i] = Tuple.Create(text1, text2);
+                        IsRepeat = true;
+                        break;
+                    }
+                }
+                if(IsRepeat == false) MyDictionary.Add(Tuple.Create(text1, text2));
                 Count = MyDictionary.Count;
-                Console.Write("Хотите ли вы продолжить?(0-нет, 1-да): ");
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.Write("Хотите ли вы продолжить?(0 - нет, 1 - да): ");
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 int choose = int.Parse(Console.ReadLine());
-                if(choose == 0) break;
-            }while(true);
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write("  ---> 1");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(" 2");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(" 3");
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+                Console.ResetColor();
+                IsRepeat = false;
+                if (choose == 0) break;
+            } while(true);
         }
         public void Show() {
-            for(int i = 0; i < MyDictionary.Count; i++) Console.WriteLine($"{MyDictionary[i].Item1} - {MyDictionary[i].Item2}");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\n*********************");
+            for (int i = 0; i < MyDictionary.Count; i++) Console.WriteLine($"{MyDictionary[i].Item1} - {MyDictionary[i].Item2}");
+            Console.WriteLine("*********************\n");
+            Console.ResetColor();
         }
-        public void SaveToFile() {
-            StreamWriter file = new StreamWriter("EngRus.log", false);
+        public void SaveToFile(string file_name) {
+            StreamWriter file = new StreamWriter(file_name + ".log", false);
             for(int i = 0; i < Count; i++)
                 file.WriteLine(MyDictionary[i].Item1 + " - " + MyDictionary[i].Item2);
             file.Close();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Словарь успешно сохранен в файл!\n");
+            Console.ResetColor();
         }
-        public void LoadFromFile() {
-            StreamReader file = new StreamReader("EngRus.log", Encoding.UTF8);
+        public void LoadFromFile(string file_name) {
+            MyDictionary.Clear();
+            StreamReader file = new StreamReader(file_name + ".log", Encoding.UTF8);
             while (file.EndOfStream != true) {
                 string buff = file.ReadLine();
                 string buff2 = buff.Substring(0, buff.IndexOf("-") - 1);
@@ -53,6 +90,71 @@ namespace Project1 {
                 MyDictionary.Add(Tuple.Create(buff2, buff3));
             }
             file.Close();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("--> Словарь успешно загружен из файла!\n");
+            Console.ResetColor();
+        }
+        public void DeleteWorld(int index) {
+            MyDictionary.RemoveAt(index);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("*****************************\n");
+            Console.WriteLine("Слово и перевод были удалены.\n");
+            Console.WriteLine("*****************************\n");
+            Console.ResetColor();
+        }
+        public void EditTranslate(int index) {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Введите новые перевод(ы) слова: ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            MyDictionary[index] = Tuple.Create(MyDictionary[index].Item1, Console.ReadLine());
+            Console.ResetColor();
+        }
+        public void EditWord(int index) {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Введите новое слово, которое нужно перевести: ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            MyDictionary[index] = Tuple.Create(Console.ReadLine(), MyDictionary[index].Item2);
+            Console.ResetColor();
+        }
+        public void SaveLineToFile(int index, string file_name) {
+            StreamWriter file = new StreamWriter(file_name + ".log", false);
+            file.WriteLine(MyDictionary[index].Item1 + " - " + MyDictionary[index].Item2);
+            file.Close();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("-->Файл успешно сохранен!\n");
+            Console.ResetColor();
+        }
+        public void FindByWord(string word) {
+            bool IsFind = false;
+            for(int i = 0; i < MyDictionary.Count; i++) {
+                if (MyDictionary[i].Item1.Contains(word)) {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write($"\nСлово было найдено под индексом {i}:");
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine($" {MyDictionary[i].Item1} - {MyDictionary[i].Item2}\n");
+                    IsFind = true;
+                    break;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (IsFind == false) Console.WriteLine("\nСлово не было найдено.");
+            Console.ResetColor();
+        }
+        public void FindByTranslate(string word) {
+            bool IsFind = false;
+            for (int i = 0; i < MyDictionary.Count; i++) {
+                if (MyDictionary[i].Item2.Contains(word)) {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write($"\nПеревод был найден под индексом {i}:");
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine($" {MyDictionary[i].Item1} - {MyDictionary[i].Item2}\n");
+                    IsFind = true;
+                    break;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (IsFind == false) Console.WriteLine("\nПеревод не был найден.");
+            Console.ResetColor();
         }
         public IEnumerator GetEnumerator() {
             for(int i = 0; i < MyDictionary.Count; i++)
--- Project1/Project1/Program.cs
+++ Project1/Project1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,155 @@ namespace Project1 {
     internal class Program {
         static void Main(string[] args) {
             EngRus obj = new EngRus();
-            obj.LoadFromFile();
-            obj.Show();
-            //obj.Input();
-            //obj.SaveToFile();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            Console.WriteLine("Здравия желаю, перед вами приложение\n\"Словарь\"разработанное @diktator_mira");
+            Console.WriteLine("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Выберите какой словарь вы хотите(Он будет автоматически загружен из файла): ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("********************************");
+            Console.WriteLine("1 - Англо-русский\n2 - Русско-английский\n3 - Украино-русский\n4 - Русско-украинский");
+            Console.WriteLine("********************************\n");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Введите действие: ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            int choose = int.Parse(Console.ReadLine());
+            do {
+                bool Znach = false;
+                switch (choose) {
+                    case 1:
+                        obj.LoadFromFile("EngRus");
+                        Znach = true;
+                        break;
+                    case 2:
+                        obj.LoadFromFile("RusEng");
+                        Znach = true;
+                        break;
+                    case 3:
+                        obj.LoadFromFile("UkrRus");
+                        Znach = true;
+                        break;
+                    case 4:
+                        obj.LoadFromFile("RusUkr");
+                        Znach = true;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Введено неправильное значение!");
+                        break;
+                }
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write("  ---> 1");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(" 2");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(" 3");
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+                if (Znach == true) break;
+            } while (true);
+            do {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("\tВыберите действие: ");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("********************************");
+                Console.WriteLine("1 - Показать словарь в консоли");
+                Console.WriteLine("2 - Добавить новые слова и перевод в словарь");
+                Console.WriteLine("3 - Сохранить словарь в файл");
+                Console.WriteLine("4 - Загрузить словарь из своего файла");
+                Console.WriteLine("5 - Удалить слово и перевод из словаря");
+                Console.WriteLine("6 - Изменить перевод в словаре");
+                Console.WriteLine("7 - Изменить слово, для перевода, в словаре");
+                Console.WriteLine("8 - Сохранить отдельную строчку в словаре в свой файл");
+                Console.WriteLine("9 - Найти строчку в словаре по слову");
+                Console.WriteLine("10 - Найти строчку в словаре по переводу");
+                Console.WriteLine("********************************");
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.Write("Выберите действие: ");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                choose = int.Parse(Console.ReadLine());
+                switch (choose) {
+                    case 1:
+                        obj.Show();
+                        break;
+                    case 2:
+                        obj.Input();
+                        break;
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("Введите имя файла для сохранения: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        obj.SaveToFile(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("Введите имя своего файла, для загрузки из него: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        obj.LoadFromFile(Console.ReadLine());
+                        break;
+                    case 5:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.WriteLine("Введите индекс строчки для удаления: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        obj.DeleteWorld(int.Parse(Console.ReadLine()));
+                        break;
+                    case 6:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("Введите индекс строчки для изменения перевода: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        obj.EditTranslate(int.Parse(Console.ReadLine()));
+                        break;
+                    case 7:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("Введите индекс строчки для изменения cлова: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        obj.EditWord(int.Parse(Console.ReadLine()));
+                        break;
+                    case 8:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("Введите имя файла, для сохранения строчки в файл: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        string name = Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("Введите индекс строчки для сохранения в файл: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        obj.SaveLineToFile(int.Parse(Console.ReadLine()), name);
+                        break;
+                    case 9:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("Введите слово, для поиска строчки: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        obj.FindByWord(Console.ReadLine());
+                        break;
+                    case 10:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write("Введите перевод, для поиска строчки: ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        obj.FindByTranslate(Console.ReadLine());
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Введено неправильное значение!\n");
+                        break;
+                }
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.Write("Хотите ли вы продолжить?(0 - нет, 1 - да): ");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                int menu = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write("  ---> 1");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(" 2");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(" 3");
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+                if (menu == 0) break;
+            } while (true);
+            Console.ResetColor();
             Console.ReadKey();
         }
     }
