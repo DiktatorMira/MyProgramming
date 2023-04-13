4a051362816ce197570b4d31966122b8d1c54652
Diktator <dychervony@gmail.com>
2023-04-13 12:03:12 +0300

    добавил дз за 13.04.2023

---
+++ Dz13.04.2023/Dz13.04.2023/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Dz13._04._2023 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.group12 = new System.Windows.Forms.GroupBox();
+            this.radio23 = new System.Windows.Forms.RadioButton();
+            this.radio22 = new System.Windows.Forms.RadioButton();
+            this.radio21 = new System.Windows.Forms.RadioButton();
+            this.group11 = new System.Windows.Forms.GroupBox();
+            this.radio13 = new System.Windows.Forms.RadioButton();
+            this.radio12 = new System.Windows.Forms.RadioButton();
+            this.radio11 = new System.Windows.Forms.RadioButton();
+            this.tab1 = new System.Windows.Forms.TabControl();
+            this.next = new System.Windows.Forms.Button();
+            this.group13 = new System.Windows.Forms.GroupBox();
+            this.radio33 = new System.Windows.Forms.RadioButton();
+            this.radio32 = new System.Windows.Forms.RadioButton();
+            this.radio31 = new System.Windows.Forms.RadioButton();
+            this.check = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage1.SuspendLayout();
+            this.group12.SuspendLayout();
+            this.group11.SuspendLayout();
+            this.tab1.SuspendLayout();
+            this.group13.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.group13);
+            this.tabPage1.Controls.Add(this.group12);
+            this.tabPage1.Controls.Add(this.group11);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(528, 309);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // group12
+            // 
+            this.group12.Controls.Add(this.radio23);
+            this.group12.Controls.Add(this.radio22);
+            this.group12.Controls.Add(this.radio21);
+            this.group12.Location = new System.Drawing.Point(256, 6);
+            this.group12.Name = "group12";
+            this.group12.Size = new System.Drawing.Size(214, 118);
+            this.group12.TabIndex = 3;
+            this.group12.TabStop = false;
+            this.group12.Text = "В какой версии windows не было Пуска?";
+            // 
+            // radio23
+            // 
+            this.radio23.AutoSize = true;
+            this.radio23.Location = new System.Drawing.Point(7, 88);
+            this.radio23.Name = "radio23";
+            this.radio23.Size = new System.Drawing.Size(42, 20);
+            this.radio23.TabIndex = 2;
+            this.radio23.TabStop = true;
+            this.radio23.Text = "11";
+            this.radio23.UseVisualStyleBackColor = true;
+            // 
+            // radio22
+            // 
+            this.radio22.AutoSize = true;
+            this.radio22.Location = new System.Drawing.Point(7, 62);
+            this.radio22.Name = "radio22";
+            this.radio22.Size = new System.Drawing.Size(45, 20);
+            this.radio22.TabIndex = 1;
+            this.radio22.TabStop = true;
+            this.radio22.Text = "8.1";
+            this.radio22.UseVisualStyleBackColor = true;
+            // 
+            // radio21
+            // 
+            this.radio21.AutoSize = true;
+            this.radio21.Location = new System.Drawing.Point(7, 36);
+            this.radio21.Name = "radio21";
+            this.radio21.Size = new System.Drawing.Size(35, 20);
+            this.radio21.TabIndex = 0;
+            this.radio21.TabStop = true;
+            this.radio21.Text = "8";
+            this.radio21.UseVisualStyleBackColor = true;
+            this.radio21.CheckedChanged += new System.EventHandler(this.radio21_CheckedChanged);
+            // 
+            // group11
+            // 
+            this.group11.Controls.Add(this.radio13);
+            this.group11.Controls.Add(this.radio12);
+            this.group11.Controls.Add(this.radio11);
+            this.group11.Location = new System.Drawing.Point(6, 6);
+            this.group11.Name = "group11";
+            this.group11.Size = new System.Drawing.Size(199, 118);
+            this.group11.TabIndex = 0;
+            this.group11.TabStop = false;
+            this.group11.Text = "Когда у нас отменили крепостное право?";
+            // 
+            // radio13
+            // 
+            this.radio13.AutoSize = true;
+            this.radio13.Location = new System.Drawing.Point(7, 88);
+            this.radio13.Name = "radio13";
+            this.radio13.Size = new System.Drawing.Size(56, 20);
+            this.radio13.TabIndex = 2;
+            this.radio13.TabStop = true;
+            this.radio13.Text = "1888";
+            this.radio13.UseVisualStyleBackColor = true;
+            // 
+            // radio12
+            // 
+            this.radio12.AutoSize = true;
+            this.radio12.Location = new System.Drawing.Point(7, 62);
+            this.radio12.Name = "radio12";
+            this.radio12.Size = new System.Drawing.Size(56, 20);
+            this.radio12.TabIndex = 1;
+            this.radio12.TabStop = true;
+            this.radio12.Text = "1861";
+            this.radio12.UseVisualStyleBackColor = true;
+            this.radio12.CheckedChanged += new System.EventHandler(this.radio12_CheckedChanged);
+            // 
+            // radio11
+            // 
+            this.radio11.AutoSize = true;
+            this.radio11.Location = new System.Drawing.Point(7, 36);
+            this.radio11.Name = "radio11";
+            this.radio11.Size = new System.Drawing.Size(56, 20);
+            this.radio11.TabIndex = 0;
+            this.radio11.TabStop = true;
+            this.radio11.Text = "1844";
+            this.radio11.UseVisualStyleBackColor = true;
+            // 
+            // tab1
+            // 
+            this.tab1.Controls.Add(this.tabPage1);
+            this.tab1.Location = new System.Drawing.Point(12, 12);
+            this.tab1.Name = "tab1";
+            this.tab1.SelectedIndex = 0;
+            this.tab1.Size = new System.Drawing.Size(536, 338);
+            this.tab1.TabIndex = 0;
+            // 
+            // next
+            // 
+            this.next.Enabled = false;
+            this.next.Location = new System.Drawing.Point(414, 352);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(130, 53);
+            this.next.TabIndex = 1;
+            this.next.Text = "Следующая страница";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // group13
+            // 
+            this.group13.Controls.Add(this.radio33);
+            this.group13.Controls.Add(this.radio32);
+            this.group13.Controls.Add(this.radio31);
+            this.group13.Location = new System.Drawing.Point(155, 144);
+            this.group13.Name = "group13";
+            this.group13.Size = new System.Drawing.Size(177, 118);
+            this.group13.TabIndex = 4;
+            this.group13.TabStop = false;
+            this.group13.Text = "На какой улице жил Шерлок Холмс?";
+            // 
+            // radio33
+            // 
+            this.radio33.AutoSize = true;
+            this.radio33.Location = new System.Drawing.Point(7, 88);
+            this.radio33.Name = "radio33";
+            this.radio33.Size = new System.Drawing.Size(116, 20);
+            this.radio33.TabIndex = 2;
+            this.radio33.TabStop = true;
+            this.radio33.Text = "Бейкер стрит";
+            this.radio33.UseVisualStyleBackColor = true;
+            this.radio33.CheckedChanged += new System.EventHandler(this.radio33_CheckedChanged);
+            // 
+            // radio32
+            // 
+            this.radio32.AutoSize = true;
+            this.radio32.Location = new System.Drawing.Point(7, 62);
+            this.radio32.Name = "radio32";
+            this.radio32.Size = new System.Drawing.Size(91, 20);
+            this.radio32.TabIndex = 1;
+            this.radio32.TabStop = true;
+            this.radio32.Text = "Хрещатик";
+            this.radio32.UseVisualStyleBackColor = true;
+            // 
+            // radio31
+            // 
+            this.radio31.AutoSize = true;
+            this.radio31.Location = new System.Drawing.Point(7, 36);
+            this.radio31.Name = "radio31";
+            this.radio31.Size = new System.Drawing.Size(143, 20);
+            this.radio31.TabIndex = 0;
+            this.radio31.TabStop = true;
+            this.radio31.Text = "Ильфа и Петрова";
+            this.radio31.UseVisualStyleBackColor = true;
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(16, 352);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(130, 53);
+            this.check.TabIndex = 2;
+            this.check.Text = "Проверить результаты";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(171, 365);
+            this.progressBar1.Maximum = 9;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(225, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(268, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0/9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 417);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.tab1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.group12.ResumeLayout(false);
+            this.group12.PerformLayout();
+            this.group11.ResumeLayout(false);
+            this.group11.PerformLayout();
+            this.tab1.ResumeLayout(false);
+            this.group13.ResumeLayout(false);
+            this.group13.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox group11;
+        private System.Windows.Forms.RadioButton radio13;
+        private System.Windows.Forms.RadioButton radio12;
+        private System.Windows.Forms.RadioButton radio11;
+        private System.Windows.Forms.TabControl tab1;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.GroupBox group12;
+        private System.Windows.Forms.RadioButton radio23;
+        private System.Windows.Forms.RadioButton radio22;
+        private System.Windows.Forms.RadioButton radio21;
+        private System.Windows.Forms.GroupBox group13;
+        private System.Windows.Forms.RadioButton radio33;
+        private System.Windows.Forms.RadioButton radio32;
+        private System.Windows.Forms.RadioButton radio31;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Dz13.04.2023/Dz13.04.2023/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dz13._04._2023 {
+    public partial class Form1 : Form {
+        int quan = 0, value = 0;
+        RadioButton radio213, radio221, radio232;
+        GroupBox group21, group22, group23, group31, group32, group33;
+        public Form1() {
+            InitializeComponent();
+            tab1.Controls[0].Text = "Страница 1";
+        }
+        private void next_Click(object sender, EventArgs e) {
+            switch (value) {
+                case 0:
+                    next.Enabled = false;
+                    check.Enabled = true;
+                    TabPage page2 = new TabPage("Страница 2");
+                    group21 = new GroupBox();
+                    group21.Location = new Point(6, 6);
+                    group21.Size = new Size(170, 118);
+                    group21.Text = "В каком году основали Microsoft?";
+                    page2.Controls.Add(group21);
+                    RadioButton radio211 = new RadioButton();
+                    radio211.Location = new Point(7, 36);
+                    radio211.Size = new Size(56, 20);
+                    radio211.Text = "1969";
+                    group21.Controls.Add(radio211);
+                    RadioButton radio212 = new RadioButton();
+                    radio212.Location = new Point(7, 62);
+                    radio212.Size = new Size(56, 20);
+                    radio212.Text = "1984";
+                    group21.Controls.Add(radio212);
+                    radio213 = new RadioButton();
+                    radio213.Location = new Point(7, 88);
+                    radio213.Size = new Size(56, 20);
+                    radio213.Text = "1975";
+                    group21.Controls.Add(radio213);
+                    radio213.CheckedChanged += new EventHandler(radio213_CheckedChanged);
+                    group22 = new GroupBox();
+                    group22.Location = new Point(190, 6);
+                    group22.Size = new Size(190, 118);
+                    group22.Text = "Какая валюта у Великобритании?";
+                    page2.Controls.Add(group22);
+                    radio221 = new RadioButton();
+                    radio221.Location = new Point(7, 36);
+                    radio221.Size = new Size(110, 20);
+                    radio221.Text = "Фунт-стерлинг";
+                    radio221.CheckedChanged += new EventHandler(radio221_CheckedChanged);
+                    group22.Controls.Add(radio221);
+                    RadioButton radio222 = new RadioButton();
+                    radio222.Location = new Point(7, 62);
+                    radio222.Size = new Size(90, 20);
+                    radio222.Text = "Гривна";
+                    group22.Controls.Add(radio222);
+                    RadioButton radio223 = new RadioButton();
+                    radio223.Location = new Point(7, 88);
+                    radio223.Size = new Size(90, 20);
+                    radio223.Text = "Форинт";
+                    group22.Controls.Add(radio223);
+                    group23 = new GroupBox();
+                    group23.Location = new Point(105, 127);
+                    group23.Size = new Size(190, 118);
+                    group23.Text = "Как называется пост премьер-министра в Германии?";
+                    page2.Controls.Add(group23);
+                    RadioButton radio231 = new RadioButton();
+                    radio231.Location = new Point(7, 36);
+                    radio231.Size = new Size(70, 20);
+                    radio231.Text = "Кайзер";
+                    group23.Controls.Add(radio231);
+                    radio232 = new RadioButton();
+                    radio232.Location = new Point(7, 62);
+                    radio232.Size = new Size(75, 20);
+                    radio232.Text = "Канцлер";
+                    group23.Controls.Add(radio232);
+                    radio232.CheckedChanged += new EventHandler(radio232_CheckedChanged);
+                    RadioButton radio233 = new RadioButton();
+                    radio233.Location = new Point(7, 88);
+                    radio233.Size = new Size(75, 20);
+                    radio233.Text = "Вермахт";
+                    group23.Controls.Add(radio233);
+                    tab1.Controls.Add(page2);
+                    value++;
+                    break;
+                case 1:
+                    next.Enabled = false;
+                    check.Enabled = true;
+                    TabPage page3 = new TabPage("Страница 3");
+                    group31 = new GroupBox();
+
+                    tab1.Controls.Add(page3);
+                    value++;
+                    break;
+            }
+        }
+        private void check_Click(object sender, EventArgs e) {
+            switch (value) {
+                case 0:
+                    List<GroupBox> list1 = new List<GroupBox>() { group11, group12, group13 };
+                    progressBar1.Value = quan;
+                    label1.Text = $"{quan}/9";
+                    foreach (GroupBox group in list1) group.Enabled = false;
+                    next.Enabled = true;
+                    check.Enabled = false;
+                    break;
+                case 1:
+                    List<GroupBox> list2 = new List<GroupBox>() { group21, group22, group23 };
+                    progressBar1.Value = quan;
+                    label1.Text = $"{quan}/9";
+                    foreach (GroupBox group in list2) group.Enabled = false;
+                    next.Enabled = true;
+                    check.Enabled = false;
+                    break;
+            }
+        }
+        private void radio12_CheckedChanged(object sender, EventArgs e) {
+            if (radio12.Checked) quan++;
+        }
+        private void radio21_CheckedChanged(object sender, EventArgs e) {
+            if (radio21.Checked) quan++;
+        }
+        private void radio33_CheckedChanged(object sender, EventArgs e) {
+            if (radio33.Checked) quan++;
+        }
+        private void radio213_CheckedChanged(object sender, EventArgs e) {
+            if (radio213.Checked) quan++;
+        }
+        private void radio221_CheckedChanged(object sender, EventArgs e) {
+            if (radio221.Checked) quan++;
+        }
+        private void radio232_CheckedChanged(object sender, EventArgs e) {
+            if (radio232.Checked) quan++;
+        }
+    }
+}
