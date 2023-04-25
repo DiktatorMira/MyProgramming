209d0268d185cabf119cae2e098e73f5f6f615f8
Diktator <dychervony@gmail.com>
2023-04-25 12:21:07 +0300

    добавил дз за 25.04.2023

---
+++ Dz25.04.2023/Dz25.04.2023/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Dz25._04._2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("JetBrains Mono", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(280, 263);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 71);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1 \r\nАБВГ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(111, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 71);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2\r\nДЕЁЖ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(212, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 71);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3\r\nЗИЙК";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(11, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 71);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4\r\nЛМНО";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(111, 358);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 71);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5\r\nПРСТ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(212, 358);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 71);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6\r\nУФХЦ";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(12, 435);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 71);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7\r\nЧШЩЪ";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(111, 435);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 71);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8\r\nЫЬЭЮ";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(212, 435);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 71);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9\r\nЯ\r\n";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(12, 512);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(80, 71);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "*";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button11.Location = new System.Drawing.Point(111, 512);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(80, 71);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "0\r\n+";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("KardinalPro ExtraBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button12.Location = new System.Drawing.Point(212, 512);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(80, 71);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "#";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 595);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Ксиоми";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Dz25.04.2023/Dz25.04.2023/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dz25._04._2023 {
+    public partial class Form1 : Form {
+        List<string> words = new List<string>();
+        List<Button> buttons;
+        public Form1() {
+            InitializeComponent();
+            using (StreamReader file = new StreamReader("dictionary.txt", Encoding.UTF8)) {
+                while (!file.EndOfStream) words.Add(file.ReadLine());
+            }
+            buttons = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e) {
+            if(!String.IsNullOrEmpty(richTextBox1.Text)) {
+                for (int i = 0; i < richTextBox1.Text.Length; i++) {
+                    foreach(string word in words) {
+                        if (i != 0 && richTextBox1.Text[i] == word[0] && richTextBox1.Text[i - 1] == ' ') {
+                            for (int j = 1; j < word.Length; j++) {
+                                richTextBox1.Text += word[j];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e) {
+            foreach (Button button in buttons) button.BackColor = Color.White;
+            switch (e.KeyCode) {
+                case Keys.F:
+                case Keys.Oemcomma:
+                case Keys.D:
+                case Keys.U:
+                    button1.BackColor = Color.Aquamarine;
+                    break;
+                case Keys.L:
+                case Keys.T:
+                case Keys.Oemtilde:
+                case Keys.Oem1:
+                    button2.BackColor = Color.Aquamarine;
+                    break;
+                case Keys.P:
+                case Keys.B:
+                case Keys.Q:
+                case Keys.R:
+                    button3.BackColor = Color.Aquamarine;
+                    break;
+                case Keys.K:
+                case Keys.V:
+                case Keys.Y:
+                case Keys.J:
+                    button4.BackColor = Color.Aquamarine;
+                    break;
+                case Keys.G:
+                case Keys.H:
+                case Keys.C:
+                case Keys.N:
+                    button5.BackColor = Color.Aquamarine;
+                    break;
+                case Keys.E:
+                case Keys.A:
+                case Keys.OemOpenBrackets:
+                case Keys.W:
+                    button6.BackColor = Color.Aquamarine;
+                    break;
+                case Keys.X:
+                case Keys.I:
+                case Keys.O:
+                case Keys.Oem6:
+                    button7.BackColor = Color.Aquamarine;
+                    break;
+                case Keys.S:
+                case Keys.M:
+                case Keys.Oem7:
+                case Keys.OemPeriod:
+                    button8.BackColor = Color.Aquamarine;
+                    break;
+                case Keys.Z:
+                    button9.BackColor = Color.Aquamarine;
+                    break;
+            }
+        }
+    }
+}
